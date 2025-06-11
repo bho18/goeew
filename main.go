8464def40c404e719ded9cfe8962551cabd64c3a
bho18 <kd7cra@gmail.com>
2025-06-10 19:24:09 -0500

    Use valid mmi functions in concurrent example

--- main.go
+++ main.go
@@ -2,15 +2,37 @@ package main
 
 import (
 	"fmt"
-	"goEEW/mmi"
+	"sync"
+
+	"goeew/mmi"
 )
 
 func main() {
 	m := 6.9
 	d := 200.0
 
-	fmt.Println("Bakun-Wentworth (1997):", mmi.BakunWentworth97(m, d))
-	fmt.Println("Atkinson-Wald (2007):", mmi.AtkinsonWald07(m, d))
-	fmt.Println("Allen-Wald (2012):", mmi.AllenWald12(m, d))
-	fmt.Println("Best Estimate:", mmi.BestEstimate(m, d))
+	var wg sync.WaitGroup
+	wg.Add(4)
+
+	go func() {
+		defer wg.Done()
+		fmt.Println("Bakun-Wentworth (1997):", mmi.BakunWentworth97(m, d))
+	}()
+
+	go func() {
+		defer wg.Done()
+		fmt.Println("Atkinson-Wald (2007):", mmi.AtkinsonWald07(m, d))
+	}()
+
+	go func() {
+		defer wg.Done()
+		fmt.Println("Allen-Wald (2012):", mmi.AllenWald12(m, d))
+	}()
+
+	go func() {
+		defer wg.Done()
+		fmt.Println("Best Estimate:", mmi.BestEstimate(m, d))
+	}()
+
+	wg.Wait()
 }
